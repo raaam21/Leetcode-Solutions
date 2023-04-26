764bcb7ccec6e2462c641c7118467ccbdb752b9f
Ram <115159988+raaam21@users.noreply.github.com>
2023-04-26 23:16:46 +0530

    Time: 31 ms (80.29%), Space: 13.8 MB (90.82%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,8 +1,8 @@
 class Solution:
     def addDigits(self, num: int) -> int:
         if num==0:
-            return 0
-        while(num>9):    
-            num=num//10+num%10
-        return(num)
+            return num
+        if num%9==0:
+            return 9
+        return(num%9)
         
